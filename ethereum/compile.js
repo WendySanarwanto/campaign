3c941f7065d34b1b8c4bbc3552c58ba0fccd8cd0
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-03-29 07:33:48 +0800

    Implement compile code.

---
+++ ethereum/compile.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+
+// Cleanup build's artefact
+fs.removeSync(buildPath);
+
+// Load and compile the CampaignFactory.sol file
+const campaignFactoryPath = path.resolve(__dirname, 'contracts', 'CampaignFactory.sol');
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+// console.log(`[DEBUG] - campaignFactoryPath: \n`, campaignFactoryPath);
+
+const campaignFactorySource = fs.readFileSync(campaignFactoryPath, 'utf8');
+const campaignSource = fs.readFileSync(campaignPath, 'utf8');
+// console.log(`[DEBUG] - campaignFactorySource: \n`, campaignFactorySource);
+
+const output = solc.compile({
+  sources: {
+    'Campaign.sol': campaignSource,
+    'CampaignFactory.sol': campaignFactorySource
+  }
+}, 1).contracts;
+
+// console.log(`[DEBUG] - output: \n`, output);
+
+// Save the compile output as .JSON files in build sub directory.
+fs.ensureDirSync(buildPath);
+
+const outputKeys = Object.keys(output);
+
+// console.log(`[DEBUG] - outputKeys: \n`, outputKeys);
+
+for(const key of outputKeys) {
+  const colonIndex = key.indexOf(':');
+  const fileName = key.substr(colonIndex+1, key.length-colonIndex) + '.json';
+  const filePath = path.resolve(buildPath, fileName);
+  fs.outputJsonSync(
+    filePath,
+    output[key]
+  );
+}
--- ethereum/contracts/CampaignFactory.sol
+++ ethereum/contracts/CampaignFactory.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 
-import "./Campaign.sol";
+import { Campaign } from "./Campaign.sol";
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
