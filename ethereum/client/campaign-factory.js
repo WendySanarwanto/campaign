7f51639ce2615b317759786974e3081466b47abe
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-01 09:51:53 +0800

    Updated campaign factory contract's address

--- ethereum/client/campaign-factory.js
+++ ethereum/client/campaign-factory.js
@@ -1,7 +1,7 @@
 import web3 from '.web3';
 import CampaignFactory from '../build/CampaignFactory.json';
 
-const CAMPAIGN_FACTORY_CONTRACT_ADDRESS = "0xabFc4F6996aE6fc0e25DaBBeBc25601e5c5EC111";
+const CAMPAIGN_FACTORY_CONTRACT_ADDRESS = "0xDDB0F5fE061c7E0657DC49Fd5e9Ee94657c16c4F";
 
 const contractInterface = new web3.eth.Contract(JSON.parse(CampaignFactory.interface), CAMPAIGN_FACTORY_CONTRACT_ADDRESS);
 
