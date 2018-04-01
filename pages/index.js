a7d2fd89d2d04abc26cdd5cb2afe17bf79eff875
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-01 14:58:08 +0800

    Added logic to get a list of deployed Campaign contract addresses.

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import campaignFactory from '../ethereum/client/campaign-factory';
 
 export default class CampaignIndex extends Component {
-  async componentDidMount() {
+
+  //#region Load the data on server side
+  
+  /**
+   * We are using Next.js. Therefore, we would like to retrieve deployed campaigns from server side.
+   */
+  static async getInitialProps() {
     const campaigns = await campaignFactory.methods.getDeployedCampaigns().call();
-    console.log(`[DEBUG] - campaigns: \n`, campaigns);
+    return { campaigns };
   }
 
+  /**
+   * Render this component.
+   */
   render() {
-    return <h1>Campaigns Index !!</h1>;
+    return <h1>{this.props.campaigns[0]}</h1>;
   }
+  
+  //#endregion
+
+  //#region Load the data on front end
+
+  // state = {
+  //   campaigns: []
+  // };
+
+  // /**
+  //  * Initialise deployed campaigns from front end side.
+  // */
+
+  // async componentDidMount() {
+  //   const campaigns = await campaignFactory.methods.getDeployedCampaigns().call();
+  //   // console.log(`[DEBUG] - campaigns: \n`, campaigns);
+  //   this.setState({campaigns});
+  // }
+  
+  // /**
+  //  * Render this component.
+  //  */
+  // render() {
+  //   return <h1>{this.state.campaigns[0]}</h1>;
+  // }
+
+  //#endregion
 }
 
