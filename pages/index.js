102f30fc33237cd98abbeeae1c96b52009be9b8a
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-02 23:46:53 +0800

    Changed the Create Campaign button's position & style by adding floated property set as right.

--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,8 @@ export default class CampaignIndex extends Component {
         <div>
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
           <h3>Open Campaign</h3>
+          <Button content="Create Campaign" icon="add circle" primary={true} floated="right"/>
           {this.renderCampaigns()}
-          <Button content="Create Campaign" icon="add circle" primary={true}/>
         </div>
       </Layout>
     );
