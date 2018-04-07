fbd9bfdb8bbcc5c35b1b7c47db47d5595b14f2d3
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-07 08:58:09 +0800

    Added a Handler for Submit button's event which calls the smart contract's createCampaign method.

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
-import Layout from '../../components/Layout';
 import { Form, Button, Input } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/client/campaign-factory';
+import web3 from '../../ethereum/client/web3';
 
 export default class CampaignNew extends Component {
   state = {
     minimumContribution: ''
   };
+
+  /**
+   * Handle form submission 
+   * Note: Arrow function is used so that we can get proper `this` reference inside.
+   */
+  onSubmit = async (event) => {
+    // Prevent the browser from submitting the form (refresh page)
+    event.preventDefault();
+
+    // Get a list accounts from the web3 instance.
+    const accounts = await web3.eth.getAccounts();
+
+    // Execute createCampaign contract method 
+    await factory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      });
+  }
 
   render() {
     return (
@@ -22,7 +43,7 @@ export default class CampaignNew extends Component {
                     this.setState({minimumContribution: event.target.value}) } />
           </Form.Field>
 
-          <Button primary>Create!</Button>
+          <Button primary onClick={ this.onSubmit }>Create!</Button>
         </Form>
       </Layout>
     );
