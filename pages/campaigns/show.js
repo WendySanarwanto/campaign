b9ebf02bed1eb0bcffa587b1319f46f31bacbd7f
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-07 14:30:39 +0800

    Put the Layout component in the CampaignShow component

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
 
 export default class CampaignShow extends Component {
     render() {
-        return <h1> Show Campaign! </h1>;
+        return (
+            <div>
+                <Layout>
+                    <h1> Show Campaign! </h1>
+                </Layout>
+            </div>
+        )
     }
 };
