c3c39b60623ed1f548b092fbc399e77ea5dc810b
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-08 21:48:11 +0800

    Implemented Finalise button and logic

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -5,56 +5,94 @@ import getCampaignContractInterface from '../ethereum/client/getCampaignContract
 import { Router } from '../routes';
 
 export default class RequestRow extends Component {
-    state = {
-        isApproving: false
+  state = {
+    isApproving: false,
+    isFinalising: false,
+  }
+
+  onApprove = async () => {
+    const { campaignAddress, id } = this.props;
+
+    try {                    
+      this.setState({isApproving: true});
+
+      // Get instance of Campaign contract's interface
+      const campaignContractInterface = await getCampaignContractInterface(campaignAddress);
+
+      // Get accounts list
+      const accounts = await web3.eth.getAccounts();
+
+      // Invoke Campaign contract's approveRequest method.
+      await campaignContractInterface.methods.approveRequest(id).send({
+        from: accounts[0]
+      });
+
+      Router.replaceRoute(`/campaigns/${campaignAddress}/requests`);
+    } catch(err){
+      console.log(`[ERROR] - Details: \n`, err);
+    } finally {
+      this.setState({isApproving: false});
     }
+  };
 
-    onApprove = async () => {
-        const { campaignAddress, id } = this.props;
+  onFinalise = async () => {
+    const { campaignAddress, id } = this.props;
 
-        try {                    
-            this.setState({isApproving: true});
+    try {                    
+      this.setState({isFinalising: true});
 
-            // Get instance of Campaign contract's interface
-            const campaignContractInterface = await getCampaignContractInterface(campaignAddress);
+      // Get instance of Campaign contract's interface
+      const campaignContractInterface = await getCampaignContractInterface(campaignAddress);
 
-            // Get accounts list
-            const accounts = await web3.eth.getAccounts();
+      // Get accounts list
+      const accounts = await web3.eth.getAccounts();
 
-            // Invoke Campaign contract's approveRequest method.
-            await campaignContractInterface.methods.approveRequest(id).send({
-                from: accounts[0]
-            });
+      // Invoke Campaign contract's approveRequest method.
+      await campaignContractInterface.methods.finaliseRequest(id).send({
+        from: accounts[0]
+      });
 
-            Router.replaceRoute(`/campaigns/${campaignAddress}/requests`);
-        } catch(err){
-            console.log(`[ERROR] - Details: \n`, err);
-        } finally {
-            this.setState({isApproving: false});
-        }
-    };
-
-    render() {
-        const { Cell, Row } = Table;
-        const { approversCount, id, request } = this.props;
-        const { approvalCount, complete, description, recipient, value } = request;
-        
-        return (
-            <Row>
-                <Cell>{id+1}</Cell>
-                <Cell>{description}</Cell>
-                <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
-                <Cell>{recipient}</Cell>
-                <Cell>{approvalCount}/{approversCount}</Cell>
-                <Cell>
-                    <Button loading={this.state.isApproving} 
-                        color="green" 
-                        basic onClick={this.onApprove}>
-                        Approve
-                    </Button>
-                </Cell>
-                <Cell></Cell>
-            </Row>
-        );
+      Router.replaceRoute(`/campaigns/${campaignAddress}/requests`);
+    } catch(err){
+      console.log(`[ERROR] - Details: \n`, err);
+    } finally {
+      this.setState({isFinalising: false});
     }
+  }
+
+  render() {
+    const { Cell, Row } = Table;
+    const { approversCount, id, request } = this.props;
+    const { approvalCount, complete, description, recipient, value } = request;
+    const readyToFinalise = approvalCount > approversCount / 2;
+    
+    return (
+      <Row disabled={complete} 
+           positive={readyToFinalise && !complete}>
+        <Cell>{id+1}</Cell>
+        <Cell>{description}</Cell>
+        <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
+        <Cell>{recipient}</Cell>
+        <Cell>{approvalCount}/{approversCount}</Cell>
+        <Cell>
+          { complete ? null : (
+            <Button loading={this.state.isApproving} 
+                color="green" 
+                basic onClick={this.onApprove}>
+                Approve
+            </Button>
+          )}
+        </Cell>
+        <Cell>
+          { complete ? null : (
+            <Button loading={this.state.isFinalising}
+                color="teal"
+                basic onClick={this.onFinalise}>
+                Finalise
+            </Button>
+          )}
+        </Cell>
+      </Row>
+    );
+  }
 };
